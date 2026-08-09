77101bee74b2c3b97ab2059822bb042913fae254
twagirumukiza <38731174+twagirumukiza@users.noreply.github.com>
2026-08-09 06:21:00 +0200

    Delete firebase-init.js

--- firebase-init.js
+++
@@ -1,2 +0,0 @@
-// La V1.3 utilise les scripts Firebase compat chargés dans index.html.
-// La configuration est intégrée dans script.js afin d'éviter les erreurs de modules ES sur GitHub Pages/iOS.
