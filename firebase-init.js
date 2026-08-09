947dc06f146f175c70c6e0c01180574d062c5f23
twagirumukiza <38731174+twagirumukiza@users.noreply.github.com>
2026-08-09 06:11:54 +0200

    Add files via upload

---
+++ firebase-init.js
@@ -0,0 +1,2 @@
+// La V1.3 utilise les scripts Firebase compat chargés dans index.html.
+// La configuration est intégrée dans script.js afin d'éviter les erreurs de modules ES sur GitHub Pages/iOS.
